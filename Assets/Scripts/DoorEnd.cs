c7f6c30ae94d162b75977dcdd6fe94b174069baa
Ксения Нагишева <knagisheva@gmail.com>
2025-04-12 23:24:44 +0300

    Doors prefabs + shading blocks in scripts + empty intro/outro scenes

---
+++ Assets/Scripts/DoorEnd.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DoorEnd : MonoBehaviour
+{
+
+    [SerializeField] private Image shade;
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(Shading());
+    }
+    
+   
+    IEnumerator Shading()
+    {
+        float a=0;
+        while (a < 1f)
+        {
+            a += 0.03f;
+            shade.color = new Color(0, 0, 0, a);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        SceneManager.LoadScene(3);
+
+    }
+}
